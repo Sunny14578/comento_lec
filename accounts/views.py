9ee2fddfe1f210d0460b4ad7e5b00dc57810e17f
Sunny14578 <imsun0101@naver.com>
2023-10-29 18:10:17 +0900

    4주차 django 개발

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from accounts.forms import SignupForm
 
+
 def signup(request):
     if request.method == "POST":
         form = SignupForm(request.POST)
